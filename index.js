fdad2698066024884befd481f89fbae82d40c2c7
arieldavis22 <aadavisbusiness@gmail.com>
2020-03-29 15:16:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    const newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return newArr
+}
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num);
+        num--;
+    }
+}
